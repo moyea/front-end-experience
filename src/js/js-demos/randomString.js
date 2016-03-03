1269ae3a4f60fda5152ed3338958e1351b820c22
qinghua <qinghua1994@hotmail.com>
2016-03-03 15:04:32 +0800

    initial media query &&normalize.css

---
+++ src/js/js-demos/randomString.js
@@ -0,0 +1,13 @@
+//生成指定长度随机字符串,默认为32位
+function randomString(len) {
+    len = len || 32;
+    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var maxPos = chars.length;
+    var randomStr = '';
+    for (i = 0; i < len; i++) {
+        randomStr += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return randomStr;
+}
+
+
